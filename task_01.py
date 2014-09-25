f0a7a8a9765a3cade1643f8a5e1d0ef6b6837ff8
apag101 <apag101@yahoo.com>
2014-09-25 17:41:48 -0400

    lesson5 task01.4

--- task_01.py
+++ task_01.py
@@ -2,31 +2,29 @@
 # -*- coding: utf-8 -*-
 """Lesson 5
 Task 01."""
-
-
-def bool_to_str(bvalue, short = False):
+def bool_to_str(bvalue, short=False):
     """Returns a boolean as a string.
 
-    Args:
-        bvalue(boolean, optional).
-	short(bolean, Default : False).
+Args:
+    bvalue(boolean, optional).
+    short(bolean, Default : False).
 
-    Returns:
-        string: Returns a string value of Yes/No/Y/N
+Returns:
+    string: Returns a string value of Yes/No/Y/N
 
-    Examples:
-        >>> task_01.bool_to_str(False)
-        'No'
-        >>> task_01.bool_to_str(True, short=True)
-        'Y'
+Examples:
+    >>> task_01.bool_to_str(False)
+    'No'
+    >>> task_01.bool_to_str(True, short=True)
+    'Y'
 
-    """
-    if bvalue == True and short == True:
-        r = 'Y'
-    elif bvalue == True and short == False:
-        r = 'Yes'
-    elif bvalue == False and short == True:
-        r = 'N'
+"""
+    if bvalue and short:
+        rt = 'Y'
+    elif bvalue and short is False:
+        rt = 'Yes'
+    elif bvalue is False and short:
+        rt = 'N'
     else:
-        r = 'No'
-    return r
+        rt = 'No'
+    return rt
