280fec75fd2f69ae0d0658bccf42fa7da9ffde19
apag101 <apag101@yahoo.com>
2014-09-27 23:06:39 -0400

    Lesson5Task2.1

--- task_02.py
+++ task_02.py
@@ -4,12 +4,21 @@
 
 import decimal
 
-#interval = 12
-#rate = None
-#total = None
-
 def get_interest_rate(principal, duration, prequalification):
-    rate = 0
+
+    """Returns interest rate or None if none exists.
+Args:
+    principal(numeric).
+    duration(numberic).
+    prequalification(boolean).
+Returns:
+    Decimal: Returns a decimal interest
+
+Examples:
+    >>> task_02.get_interest_rate(1000000, 15, True)
+    '0.0205'
+"""
+    rate = None
     if principal >= 0 and principal <= 199999:
         if duration >= 1 and duration <= 15:
             if prequalification:
@@ -47,11 +56,29 @@ def get_interest_rate(principal, duration, prequalification):
         elif duration >= 16 and duration <= 20:
             if prequalification:
                 rate = '0.0262'
-    else: rate = None
-    rate = decimal.Decimal(rate)
+    else:
+        rate = None
+        if rate is not None:
+            rate = decimal.Decimal(rate)
+        else:
+            rate = None
     return rate
 
 def compound_interest(principal, duration, rate, interval = 12):
+
+    """Returns interest and principal combined.
+Args:
+    principal(numeric).
+    duration(numberic).
+    rate(Decimal)
+    interval(numeric, Default: 12)
+Returns:
+    Integer: Returns an integer interest and principal combined.
+
+Examples:
+    >>> task_02.compound_interest(1000000, 15, True)
+    Decimal('1807919656619.745219860645100')
+"""
     if rate is not None:
         rate = decimal.Decimal(rate)
         compound = principal * ((1 + rate / interval) ** (interval * duration))
@@ -60,5 +87,38 @@ def compound_interest(principal, duration, rate, interval = 12):
     return compound
 
 def calculate_total(principal, duration, prequalification):
+
+    """Returns the total, rounded to the nearest integer.
+Args:
+    principal(numeric).
+    duration(numberic).
+    prequalification(boolean).
+Returns:
+    Integer: Returns total to the nearest integer.
+
+Examples:
+    >>> task_02.calculate_total(100000, 15, True)
+    172233
+"""
     rate = get_interest_rate(principal, duration, prequalification)
-    return rate
+    total = int(compound_interest(principal, duration, rate, interval = 12))
+    return total
+
+def calculate_interest(principal, duration, prequalification):
+
+    """Returns the total interest.
+Args:
+    principal(numeric).
+    duration(numberic).
+    prequalification(boolean).
+Returns:
+    Integer: Returns total interest.
+
+Examples:
+    >>> task_02.calculate_interest(100000, 15, True)
+    72233
+"""
+    rate = get_interest_rate(principal, duration, prequalification)
+    total = int(calculate_total(principal, duration, prequalification))
+    interest = total - principal
+    return interest
